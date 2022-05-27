96e872fe62e9ce94014894ca52ea88ead82a16aa
Luke Dimech <68693858+ludimech@users.noreply.github.com>
2022-05-27 23:18:25 +0200

    commit 2

---
+++ Lessons/Tasks/table.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("| Name\t\t | Age\t | Month of Birth |\n");
+    printf("| Luke\t | 19\t | February |\n");
+    printf("| Kristina\t | 20\t | August |\n");
+    printf("| Mia\t | 19\t | May |\n");
+    printf("| Karl\t | 21\t | January |");
+
+    return 0;
+}
